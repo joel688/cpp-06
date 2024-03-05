6f00c62bb9a936f1686e7cd5adbcfce4dd8354d5
Joachim Koenig <joakoeni@k0r1p13.42mulhouse.fr>
2024-03-05 16:52:25 +0100

    basic_update

--- ex00/Class/ScalarConverter/ScalarConverter.hpp
+++ ex00/Class/ScalarConverter/ScalarConverter.hpp
@@ -3,10 +3,14 @@
 
 #include <iostream>
 
+
+// try catch exception of stoi [] stof stod | std::invalid_argument ... watch doc
+
 class ScalarConverter
 {
 	private:
 		ScalarConverter();
+		ScalarConverter(const ScalarConverter &src);
 		~ScalarConverter();
 	public:
 		static void convert(const std::string);
